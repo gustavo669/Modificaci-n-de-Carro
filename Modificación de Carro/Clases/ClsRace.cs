53cdb297fb3e25d8fa7c1a8ba27058404bf3e2d5
barre <barre@PC-GUSTAVO>
2022-04-01 23:06:37 -0600

    Agregar archivos de proyecto.

---
+++ Modificación de Carro/Clases/ClsRace.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificación_de_Carro.Clases
+{
+    internal class ClsRace
+    {
+        private Carro CarroA;
+        private Carro CarroB;
+        private Carro CarroC;
+
+
+        public void PedirDatosCarrera()
+        {
+
+            CarroA = GetData();
+            Console.WriteLine("Ahora datos del B");
+            CarroB = GetData();
+            Console.WriteLine("Ahora datos del C");
+            CarroC = GetData();
+        }
+
+        public void IniciarCarrera()
+        {
+            IniciarCarrera(CarroA);
+        }
+
+        public void IniciarCarrera(Carro carroA)
+        {
+            string msgCarroA, msgCarroB, msgCarroC;
+
+            Random velRandom = new Random();
+
+            msgCarroA = CarroA.EncenderMotor();
+            msgCarroB = CarroB.EncenderMotor();
+            msgCarroC = CarroC.EncenderMotor();
+            Console.WriteLine($"{CarroA.Marca} {msgCarroA}");
+            Console.WriteLine($"{CarroB.Marca} {msgCarroB}");
+            Console.WriteLine($">{CarroC.Marca} {msgCarroC}");
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                int acelera;
+                acelera = velRandom.Next(1, 21);
+                msgCarroA = CarroA.acelerar(acelera);
+                acelera = velRandom.Next(1, 21);
+                msgCarroB = CarroB.acelerar(acelera);
+                acelera = velRandom.Next(1, 21);
+                msgCarroC = CarroC.acelerar(acelera);
+
+
+                Console.WriteLine($"{i + 1}- {CarroA.owner}:{msgCarroA}  {CarroB.owner}:{msgCarroB} {CarroC.owner}:{msgCarroC}");
+
+            }
+
+            if (CarroA.GetVelocidadActual() > CarroB.GetVelocidadActual())
+            {
+                Console.WriteLine($"{CarroA.owner} Wins!!!");
+            }
+            else
+            {
+                Console.WriteLine($"{CarroB.owner} Wins!!!");
+            }
+
+            if (CarroC.GetVelocidadActual() > CarroA.GetVelocidadActual())
+            {
+                Console.WriteLine($"{CarroC.owner} Wins!!!");
+            }
+            else
+            {
+                Console.WriteLine($"{CarroA.owner} Wins!!!");
+            }
+        }
+
+
+        private Carro GetData()
+        {
+            string marca;
+            int modelo;
+            int velocidad;
+            string propietario;
+            Carro carroTemporal;
+
+
+            Console.WriteLine("Ingrese marca");
+            marca = Console.ReadLine();
+
+            Console.WriteLine("ingrese modelo");
+            modelo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese velocidad max");
+            velocidad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Propietario:");
+            propietario = Console.ReadLine();
+            carroTemporal = new Carro(marca, modelo, velocidad, propietario);
+            return carroTemporal;
+        }
+    }
+}
---
+++ Modificación de Carro/Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+using Modificación_de_Carro.Clases;
+
+int mangas = 3;
+
+ClsRace carrera = new ClsRace();
+carrera.PedirDatosCarrera();
+
+for (int i = 0; i < mangas; i++)
+{
+    Console.WriteLine($"Manga {i + 1}");
+    carrera.IniciarCarrera();
+    Console.WriteLine($"--------------------------------------");
+}
+
+
+// moficar el programa para que lleve el control de las mangas que va ganando por fase
+// comentar los cambios que deben hacer 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Carro miCarrito = new Carro("Toyota",2022,"azul");
+//Carro EstefaniCarrito = new Carro("Ferrari", 2022, "Rojo");
+
+//String mensaje;
+//mensaje = EstefaniCarrito.EncenderMotor();
+
+//Console.WriteLine(mensaje);
+
+
+//if (miCarrito.Modelo > EstefaniCarrito.Modelo)
+//{
+//    Console.WriteLine("El carro del profe es mas reciente");
+//}
+//else
+//{
+//    Console.WriteLine("El carro de Estefani es mas reciente");
+//}
+
+
+////Console.WriteLine($"teacher car is {miCarrito.Marca}");
+//Console.WriteLine($"Estafani's car is {EstefaniCarrito.Marca}");
+////Console.WriteLine($"Estafani's model car is {EstefaniCarrito.Modelo}");
+
+
+////EstefaniCarrito.EncenderMotor();
+//Console.WriteLine(EstefaniCarrito.GetVelocidadActual());
+//EstefaniCarrito.EncenderMotor();
+
+//for(int i = 0; i < 5; i++)
+//{
+//EstefaniCarrito.acelerar();
+//}
+
+//EstefaniCarrito.acelerar(3);
+//EstefaniCarrito.acelerar(6);
+//EstefaniCarrito.acelerar(1);
